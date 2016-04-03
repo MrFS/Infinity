a3116d821985d9c50ea04813c8b181c510a17e47
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-04-03 12:16:46 +0200

    lul

---
+++ Infinity/Infinity/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Dim int As Integer
+    Sub Main()
+        Do
+            Console.Clear()
+            int += 1
+            Console.WriteLine(int)
+        Loop While True
+    End Sub
+
+End Module
